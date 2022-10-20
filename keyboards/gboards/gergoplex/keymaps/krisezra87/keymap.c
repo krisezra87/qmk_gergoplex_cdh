46781557626b295e08982c7e2db5f24ca479863c
Kris Ezra <kris.ezra@gmail.com>
2022-10-20 16:20:28 -0400

    Added homerow keys to the numbers layer

--- keyboards/gboards/gergoplex/keymaps/krisezra87/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/krisezra87/keymap.c
@@ -58,21 +58,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /* Keymap 2: Numbers/Motion layer
      * NEED TAPDANCE TO ADD ALT TO NUMBERS ON HOLD
-     * ,-------------------------------.      ,-------------------------------.
-     * |   1   |  2  |  3  |  4  |  5  |      |  6  |  7  |  8  |  9  |   0   |
-     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
-     * |       |     |LCTL |LALT |     |      | LFT | DWN | UP  | RGT |       |
-     * |-------+-----+-----+-----+-----|      |-----+-----+-----+-----+-------|
-     * |       | RMB | MMB | LMB |     |      | MLFT| MDWN| MUP | MRGT|       |
-     * `-------------------------------'      `-------------------------------'
+     *
+     * ,-------------------------------.      ,---------------------------------------.
+     * |   1   |  2  |  3  |  4  |  5  |      |  6  |    7    |    8    |  9  |   0   |
+     * |-------+-----+-----+-----+-----|      |-----+---------+---------+-----+-------|
+     * |       |     |LCTL |LALT |     |      | LFT | ALT DWN | CTRL UP | RGT |       |
+     * |-------+-----+-----+-----+-----|      |-----+---------+---------+-----+-------|
+     * |       | RMB | MMB | LMB |     |      | MLFT|  MDWN   |  MUP    | MRGT|       |
+     * `-------------------------------'      `---------------------------------------'
      *            .--------------------.      .-----------------.
      *            |SPECIAL|ALPHA|      |      |     |     |     |
      *            '--------------------'      '-----------------'
      */
     [_NUMBERS] = LAYOUT_split_3x5_3(
-         KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , /**/ KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,
-         KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_TRNS, /**/ KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_TRNS,
-         KC_TRNS, KC_BTN2, KC_BTN3, KC_BTN1, KC_TRNS, /**/ KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_TRNS,
+         KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , /**/ KC_6   , KC_7           , KC_8         , KC_9   , KC_0   ,
+         KC_TRNS, KC_TRNS, KC_LCTL, KC_LALT, KC_TRNS, /**/ KC_LEFT, RALT_T(KC_DOWN), RALT_T(KC_UP), KC_RGHT, KC_TRNS,
+         KC_TRNS, KC_BTN2, KC_BTN3, KC_BTN1, KC_TRNS, /**/ KC_MS_L, KC_MS_D        , KC_MS_U      , KC_MS_R, KC_TRNS,
                    TO(_SPECIAL), TO(_ALPHA), KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
