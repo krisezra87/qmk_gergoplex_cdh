daa4c272258482733c1f4ab3914f115ed84d998c
Kris Ezra <kris.ezra@gmail.com>
2022-10-30 15:30:07 -0400

    Swapping thumb keys for advancing layers

--- keyboards/gboards/gergoplex/keymaps/krisezra87/keymap.c
+++ keyboards/gboards/gergoplex/keymaps/krisezra87/keymap.c
@@ -57,13 +57,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *               .-----------------.      .----------------.
      *               |SPECIAL|ESC |BSPC|      |SPC|ENT META|ESC|
      *               '-----------------'      '----------------'
-     *               TODO: Consider esc key to be always esc on tap always alpha layer on hold
      */
+
     [_ALPHA] = LAYOUT_split_3x5_3(
          KC_Q        , KC_W      , KC_F        , KC_P        , KC_B      , /**/ KC_J, KC_L        , KC_U        , KC_Y  , KC_SCLN,
          KC_A        , KC_R      , LCTL_T(KC_S), LALT_T(KC_T), KC_G      , /**/ KC_M, RALT_T(KC_N), RCTL_T(KC_E), KC_I  , KC_O,
          LSFT_T(KC_Z), LT(0,KC_X), LT(0,KC_C)  , KC_D        , LT(0,KC_V), /**/ KC_K, KC_H        , KC_COMMA    , KC_DOT, RSFT_T(KC_SLSH),
-                                           OSL(_SPECIAL), LT(0,KC_ESC), KC_BSPC, /**/ KC_SPC, LGUI_T(KC_ENT), KC_ESC),
+                                           LT(0,KC_ESC), OSL(_SPECIAL), KC_BSPC, /**/ KC_SPC, LGUI_T(KC_ENT), KC_ESC),
 
 
     /* Keymap 1: Special characters layer
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          KC_EXLM       , KC_AT  , KC_HASH   , KC_DLR , KC_PERC, /**/ KC_CIRC, KC_AMPR, KC_ASTR   , KC_MINS, KC_BSLS,
          KC_TAB        , KC_TRNS, S(KC_LBRC), KC_LPRN, KC_LBRC, /**/ KC_RBRC, KC_RPRN, S(KC_RBRC), KC_TRNS, KC_QUOT,
          LSFT_T(KC_GRV), KC_TRNS, KC_LCTL   , KC_LALT, KC_DEL , /**/ KC_TRNS, KC_TRNS, KC_TRNS   , KC_MINS, RSFT_T(KC_EQL),
-                                             TO(_NUMBERS), TO(_ALPHA), KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS),
+                                             TO(_ALPHA), TO(_NUMBERS), KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS),
 
     /* Keymap 2: Numbers/Motion layer
      * FIX UP LEFT HAND
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
          TD(DWM_1), TD(DWM_2), TD(DWM_3)      , TD(DWM_4)    , TD(DWM_5), /**/ TD(DWM_6), TD(DWM_7)   , TD(DWM_8)   , TD(DWM_9), TD(DWM_0),
          KC_TRNS  , KC_LEFT  , LCTL_T(KC_DOWN), LALT_T(KC_UP), KC_RIGHT , /**/ KC_H     , KC_J, KC_K, KC_L     , KC_TRNS,
          KC_TRNS  , KC_BTN2  , KC_BTN3        , KC_BTN1      , KC_TRNS  , /**/ KC_MS_L  , KC_MS_D     , KC_MS_U     , KC_MS_R  , KC_TRNS,
-                                       TO(_SPECIAL), TO(_ALPHA), KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS)
+                                       TO(_ALPHA), TO(_SPECIAL), KC_TRNS, /**/ KC_TRNS, KC_TRNS, KC_TRNS)
 };
 
 /* Abuse the layer trigger function to sort out hold vs tap for some keys */
